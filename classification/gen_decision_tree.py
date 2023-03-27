01e0e825988032a57b7efe63adc12cc1b6666d6a
Adrian Stokwisz <stokwiszadrian@gmail.com>
2023-03-28 00:56:56 +0200

    data fetching optimalization

--- classification/gen_decision_tree.py
+++ classification/gen_decision_tree.py
@@ -3,6 +3,7 @@ import sys
 import json
 import psycopg2 as pg
 from id3 import id3
+from memory_profiler import profile
 
 sys.setrecursionlimit(10000)
 
@@ -18,8 +19,8 @@ def castDate(value, cur):
         return None
 
 
-if __name__ == "__main__":
-
+# @profile
+def profile_func():
     DATE = pg.extensions.new_type((1082,), "DATE", castDate)
     pg.extensions.register_type(DATE)
 
@@ -32,7 +33,7 @@ if __name__ == "__main__":
 
     # ------ Ilość rekordów pobieranych "z wierzchu"
 
-    num_entries = 1000000
+    num_entries = 100000
 
     conn = pg.connect(
         dbname=DB_NAME,
@@ -42,16 +43,16 @@ if __name__ == "__main__":
     )
 
     columnQuery = """
-            select table_schema, table_name, column_name 
-            from information_schema.columns 
-            WHERE table_schema = 'public' 
-            AND table_name = 'humans';
-            """
+                select table_schema, table_name, column_name 
+                from information_schema.columns 
+                WHERE table_schema = 'public' 
+                AND table_name = 'humans';
+                """
 
     humansquery = f"""
-            select * from humans
-            order by length(qid)
-            limit {num_entries};"""
+                select * from humans
+                order by length(qid)
+                limit {num_entries};"""
 
     # ------ Pobieranie nazw kolumn
 
@@ -71,17 +72,27 @@ if __name__ == "__main__":
     cur = conn.cursor(name="humanscursor")
     cur.execute(humansquery)
 
-    result = cur.fetchall()
-    class_list += list(map(lambda x: x[0], result))
-    for entry in result:
-        all[entry[0]] = {}
-        for j in range(2, len(features) + 2):
-            if entry[j] is not None:
-                all[entry[0]][features[j - 2]] = entry[j]
-    del result
+    print("SIZE OF CUR", sys.getsizeof(cur))
+
+    print("SIZE OF RESULT", sys.getsizeof(result))
+    print("SIZE OF CUR", sys.getsizeof(cur))
+    for v in range(0, 100):
+        print(v)
+        result = cur.fetchmany(1000)
+        class_list += list(map(lambda x: x[0], result))
+        for entry in result:
+            all[entry[0]] = {}
+            for j in range(2, len(features) + 2):
+                if entry[j] is not None:
+                    all[entry[0]][features[j - 2]] = entry[j]
+        del result
+    print("SIZE OF ALL", sys.getsizeof(all))
+    print("SIZE OF Q23", sys.getsizeof(all["Q23"]))
     gc.collect()
     print("Human entries fetched")
-
+    print("SIZE OF class_list", sys.getsizeof(class_list))
+    print("SIZE OF features", sys.getsizeof(features))
+    print("SIZE OF conn", sys.getsizeof(conn))
     decision_tree = id3(all, "qid", class_list, features)
     print("Decision tree generated")
     with open("../resources/decision_tree.json", "w") as f:
@@ -89,3 +100,8 @@ if __name__ == "__main__":
 
     cur.close()
     conn.close()
+
+
+if __name__ == "__main__":
+    profile_func()
+
--- pg/dbimport.bat
+++ pg/dbimport.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 echo Downloading database dump...
-::curl -o "../resources/projectdb_export" https://storage.googleapis.com/q20bucket/projectdb_export
+curl -o "../resources/projectdb_export" https://storage.googleapis.com/q20bucket/projectdb_export
 echo Database dump downloaded
 echo Downloading decision tree...
-::curl -o "../resources/decision_tree.json" https://storage.googleapis.com/q20bucket/decision_tree_1m.json
+curl -o "../resources/decision_tree.json" https://storage.googleapis.com/q20bucket/decision_tree_1m.json
 echo Decision tree downloaded
 echo Creating postgres container...
 docker run --name projectpg -e POSTGRES_PASSWORD=tajne -d -p 5432:5432 postgres
